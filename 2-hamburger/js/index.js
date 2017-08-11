414a5dfbce521e6c0cdbdc831176e510ce2784fd
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-08-11 11:27:54 -0500

    (feat) Added transforming hamburger menu icon

---
+++ 2-hamburger/js/index.js
@@ -0,0 +1,32 @@
+var topBar = document.querySelector('.bars__first');
+var midBar = document.querySelector('.bars__second');
+var thirdBar = document.querySelector('.bars__third');
+var bars = document.querySelector('.bars');
+
+var removeClass = function(elem, className) {
+  var regex = new RegExp("\\s?"+className);
+  elem.className = elem.className.replace(regex,"");
+}
+
+var addClass = function(elem, className) {
+    elem.className += " " + className;
+}
+
+var toggleActiveClass = function(elem) {
+  if(elem.className.indexOf("active") > -1) {
+    removeClass(elem,"active");
+  } else {
+    addClass(elem,"active");
+  }
+}
+
+bars.addEventListener('click',function(){
+  console.log("hey");
+  toggleActiveClass(topBar);
+  toggleActiveClass(midBar);
+  toggleActiveClass(thirdBar);
+  removeClass(topBar,"no-anim");
+  removeClass(midBar,"no-anim");
+  removeClass(thirdBar,"no-anim");
+});
+
